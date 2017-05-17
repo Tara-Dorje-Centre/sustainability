13344bff7a7f5db1c679117e202685ab1434460e
Anthony Harper <gaiansentience@gmail.com>
2017-05-16 18:30:21 -0700

    redirect code needed

--- index.php
+++ index.php
@@ -1,5 +1,20 @@
 <?php
-//use public site as default
-include(".php/public.php");
+
+//the .php directory should be considered a library path
+//executing from here creates
+// pretty but invalid links that dont show the resource path
+// like sysLogin.php, public.php
+// 
+// .htaccess or some kind of php configuration is needed
+//  link to public.php should bring up .php/public.php
+//  without showing the user the .php/ directory
+
+include(".php/index.php");
+
+//   current code assumes all php scripts are in docroot
+//   if working code is in htdocs/dev/sustainability
+//   use [server]/dev/sustainability/.php/index.php
+//   then all links will work without any redirects
+
 
 ?>
