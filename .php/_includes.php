4f6ebe24247ca23eabed8c269e9157520b979a83
Anthony Harper <gaiansentience@gmail.com>
2017-08-19 13:27:10 -0700

    new html formatting objects now linked to manifest.  inputs renamed to input type without underscores.  base classes separated with double underscore filenames

--- .php/_includes.php
+++ .php/_includes.php
@@ -2,7 +2,7 @@
 session_start(); 
 //echo 'started session from _includes.php<br />';
 //session_regenerate_id(TRUE);
-include_once("__manifest.inc");
+include_once("manifest.inc");
 //finish standard include process by opening active db connection
 $pathFramework = './framework/';
 $pathEntities = './entities/';
