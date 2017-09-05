a9be1aa6f0d52a0a1497344dfc038dd48c1e3355
Anthony Harper <gaiansentience@gmail.com>
2017-09-04 17:15:29 -0700

    namespace application

--- .php/_includes.php
+++ .php/_includes.php
@@ -2,11 +2,14 @@
 session_start(); 
 //echo 'started session from _includes.php<br />';
 //session_regenerate_id(TRUE);
-include_once("manifest.inc");
-//finish standard include process by opening active db connection
-$pathFramework = './framework/';
-$pathEntities = './entities/';
-include_once($pathFramework."_dbconnect.php");
+include_once("systemManifest.inc");
+
+	global $manifest;
+	$manifest = new systemManifest();
+	$manifest->initialize();
+
+
+
 
  function sessionVariableGET($id,$default){
 if (isset($_GET[$id])){		
