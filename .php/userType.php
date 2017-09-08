ef921ddc2480f178ad9f5dcfb92e47caded2d859
Anthony Harper <gaiansentience@gmail.com>
2017-09-08 13:39:56 -0700

    organization

---
+++ .php/userType.php
@@ -0,0 +1,33 @@
+﻿<?php 
+include_once("_includes.php");
+
+$context = sessionVariableGET('context','entity');
+$scope = sessionVariableGET('scope','list');
+
+switch ($scope){
+	case 'detail':
+		$action = sessionVariableGET('pageAction','VIEW');
+		$id = sessionVariableGET('id',0);
+		$t = new UserType($action,$id);
+		break;
+	case 'save':
+		$um = new UserType;
+		$um->collectPostValues();
+		$um->saveChanges();
+		//omit break and print default list
+		//break
+	case 'list':
+		//omit break and print default list
+		//break;
+	default:
+		$t = new UserTypeList();
+		$page = sessionVariableGET('resultsPage', 1);
+		$details = 10;
+		$t->setPaging($page, $details);
+}
+
+$t->getRequestArguments();
+$t->setDetails();
+$t->printPage();
+
+?>
