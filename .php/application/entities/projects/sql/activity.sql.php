48a3927dafb060471ce4dfaaef986a892c068010
Anthony Harper <gaiansentience@gmail.com>
2021-10-15 16:05:02 -0400

    cleanup

--- .php/application/entities/projects/sql/activity.sql.php
+++ .php/application/entities/projects/sql/activity.sql.php
@@ -4,13 +4,11 @@ namespace application\entities\projects\sql;
 class activitySQL 
 extends \application\sql\entitySQL{
 
-
 	public function __construct(){
 		$this->baseTable = 'activities';
 		$this->typeTable = 'activity_types';
 		$this->fieldDate = 'started';
 	}
-
 
 	protected function cols($first = true){
 		$c = $this->select($first);
@@ -45,8 +43,6 @@ extends \application\sql\entitySQL{
 		$q .= " SUM(a.hours_actual) total_hours_actual ";
 		return $q;
 	}
-
-
 
 	public function tables($joinTypes = true){
 		$q = " FROM activities a ";
@@ -100,7 +96,6 @@ extends \application\sql\entitySQL{
 		return $q;
 	}
 
-
 	public function countTask($id){
 		$q = "SELECT count(*) as count_details ";
 		$q .= "FROM activities a ";
@@ -109,23 +104,21 @@ extends \application\sql\entitySQL{
 	}
 
 	public function countProject($id){
-		$q = "SELECT count(*) as count_details ";
-		$q .= "FROM activities a ";
+		$q = " SELECT count(*) as count_details ";
+		$q .= " FROM activities a ";
 		$q .= " JOIN tasks t ON a.task_id = t.id ";
-		$q .= "WHERE t.project_id = ".$id;
+		$q .= " WHERE t.project_id = ".$id;
 		return $q;
 	}
 
-
 	public function countDoneBy($doneBy = 'EVERYONE'){
-		$q = "SELECT count(*) as count_details ";
-		$q .= "FROM activities a ";
+		$q = " SELECT count(*) as count_details ";
+		$q .= " FROM activities a ";
 		if ($doneBy <> 'EVERYONE'){
-		$q .= "WHERE UPPER(a.done_by) = UPPER('".$doneBy."') ";
+		$q .= " WHERE UPPER(a.done_by) = UPPER('".$doneBy."') ";
 		}
 		return $q;
 	}
-
 
 	public function info($id = 0){
 		$q = $this->cols();
@@ -134,7 +127,6 @@ extends \application\sql\entitySQL{
 		return $q;
 	}
 
-	
 	public function listTask($id = 0, $page = 1, $rows =10){
 		$q = $this->cols();
 		$q .= $this->tables(true);
@@ -153,11 +145,11 @@ extends \application\sql\entitySQL{
 		return $q;
 	}
 
-public function listDoneBy($doneBy = 'EVERYONE', $page = 1, $rows =10){
+	public function listDoneBy($doneBy = 'EVERYONE', $page = 1, $rows =10){
 		$q = $this->cols();
 		$q .= $this->tables(true);
 		if ($doneBy <> 'EVERYONE'){
-		$q .= "WHERE UPPER(a.done_by) = UPPER('".$doneBy."') ";
+			$q .= "WHERE UPPER(a.done_by) = UPPER('".$doneBy."') ";
 		}
 		$q .= " ORDER BY t.task_order, a.activity_order ";
 		$q .= $this->limit($page, $rows);
@@ -177,9 +169,9 @@ public function listDoneBy($doneBy = 'EVERYONE', $page = 1, $rows =10){
 		$q .= $this->tables(false);
 		$q .= " WHERE a.task_id = ".$id;
 		if ($doneBy != 'EVERYONE'){
-			$q .= $this->_equalDoneBy($doneBy);
+			$w = " AND UPPER(a.done_by) = UPPER('".$doneBy."') ";
 		} else {
-			$q .= " GROUP BY ".$this->fieldDoneBy." ";
+			$q .= " GROUP BY a.done_by ";
 		}
 		return $q;
 	}
@@ -202,57 +194,54 @@ public function listDoneBy($doneBy = 'EVERYONE', $page = 1, $rows =10){
 				}
 			}
 		}
-		
 		return $w;
 	}
 	
-	
 	public function calendarLinksDoneBy($doneBy = 'EVERYONE'){
-$q = "SELECT  ";
-$q .= " MONTH(a.started) month, ";
-$q .= " YEAR(a.started) year ";
-$q .= " FROM activities a ";
-
-$q .= $this->whereDoneByYearMonth($doneBy,0,0);
-$q .= " GROUP BY  ";
-$q .= " MONTH(a.started), ";
-$q .= " YEAR(a.started) ";
+		$q = "SELECT  ";
+		$q .= " MONTH(a.started) month, ";
+		$q .= " YEAR(a.started) year ";
+		$q .= " FROM activities a ";
+		$q .= $this->whereDoneByYearMonth($doneBy,0,0);
+		$q .= " GROUP BY  ";
+		$q .= " MONTH(a.started), ";
+		$q .= " YEAR(a.started) ";
 		return $q;
 	}
 
-public function calendarSummaryDoneBy($doneBy, $year, $month){
-$q = "SELECT  ";
-$q .= " SUM(a.hours_actual) sum_hours, ";
-$q .= " DATE(a.started) started, ";
-$q .= " a.done_by, ";
-$q .= " p.id, ";
-$q .= " min(p.name) name, ";
-$q .= " 1 ordering, ";
-$q .= " min(tt.highlight_style) highlight_style ";
-$q .= $this->tables();
-$q .= $this->whereDoneByYearMonth($doneBy,$year,$month);
-$q .= " GROUP BY  ";
-$q .= " DATE(a.started), ";
-$q .= " a.done_by, ";
-$q .= " p.id ";
-$q .= " UNION ALL ";
-$q .= " SELECT  ";
-$q .= " SUM(a.hours_actual) sum_hours, ";
-$q .= " LAST_DAY(a.started) started, ";
-$q .= " 'MonthTotal' done_by, ";
-$q .= " 0 id, ";
-$q .= " 'MonthTotal' name, ";
-$q .= " 100 ordering, ";
-$q .= " 'highlight-yellow' highlight_style ";
-$q .= $this->tables();
-$q .= $this->whereDoneByYearMonth($doneBy,$year,$month);
-$q .= " GROUP BY ";
-$q .= " LAST_DAY(a.started) ";
-$q .= " ORDER BY started, ordering, id, done_by ";
-return $q;	
-}
+	public function calendarSummaryDoneBy($doneBy, $year, $month){
+		$q = "SELECT  ";
+		$q .= " SUM(a.hours_actual) sum_hours, ";
+		$q .= " DATE(a.started) started, ";
+		$q .= " a.done_by, ";
+		$q .= " p.id, ";
+		$q .= " min(p.name) name, ";
+		$q .= " 1 ordering, ";
+		$q .= " min(tt.highlight_style) highlight_style ";
+		$q .= $this->tables();
+		$q .= $this->whereDoneByYearMonth($doneBy,$year,$month);
+		$q .= " GROUP BY  ";
+		$q .= " DATE(a.started), ";
+		$q .= " a.done_by, ";
+		$q .= " p.id ";
+		$q .= " UNION ALL ";
+		$q .= " SELECT  ";
+		$q .= " SUM(a.hours_actual) sum_hours, ";
+		$q .= " LAST_DAY(a.started) started, ";
+		$q .= " 'MonthTotal' done_by, ";
+		$q .= " 0 id, ";
+		$q .= " 'MonthTotal' name, ";
+		$q .= " 100 ordering, ";
+		$q .= " 'highlight-yellow' highlight_style ";
+		$q .= $this->tables();
+		$q .= $this->whereDoneByYearMonth($doneBy,$year,$month);
+		$q .= " GROUP BY ";
+		$q .= " LAST_DAY(a.started) ";
+		$q .= " ORDER BY started, ordering, id, done_by ";
+		return $q;	
+	}
 
-	public function whereProject($id, $year, $month){
+	public function whereProjectYearMonth($id, $year, $month){
 		$w = " WHERE t.project_id = ".$id." ";
 		if ($year > 0){
 			$w .= " AND YEAR(a.started) = ".$year." ";
@@ -260,59 +249,57 @@ return $q;
 				$w .= " AND MONTH(a.started) = ".$month." ";
 			}
 		}
-
 		return $w;
 	}
 
 
-public function calendarLinksProject($id){
-
-$q = "SELECT  ";
-$q .= " MONTH(a.started) month, ";
-$q .= " YEAR(a.started) year ";
-$q .= $this->tables();
-$q .= $this->whereProject($id, 0, 0);
-$q .= " GROUP BY ";
-$q .= " YEAR(a.started), ";
-$q .= " MONTH(a.started) ";
-$q .= " ORDER BY year, month ";
-return $q;	
-}
+	public function calendarLinksProject($id){
+		$q = "SELECT  ";
+		$q .= " MONTH(a.started) month, ";
+		$q .= " YEAR(a.started) year ";
+		$q .= $this->tables();
+		$q .= $this->whereProjectYearMonth($id, 0, 0);
+		$q .= " GROUP BY ";
+		$q .= " YEAR(a.started), ";
+		$q .= " MONTH(a.started) ";
+		$q .= " ORDER BY year, month ";
+		return $q;	
+	}
 			
-public function calendarSummaryProject($id = 0, $year = 0, $month = 0){
-//show daily tally by task and person
-$q = "SELECT  ";
-$q .= " SUM(a.hours_actual) sum_hours, ";
-$q .= " DATE(a.started) started, ";
-$q .= " a.done_by, ";
-$q .= " t.id, ";
-$q .= " min(t.name) name, ";
-$q .= " min(t.task_order) ordering, ";
-$q .= " min(tt.highlight_style) highlight_style ";
-$q .= $this->tables();
-$q .= $this->whereProject($id, $year, $month);
-$q .= " GROUP BY  ";
-$q .= " DATE(a.started), ";
-$q .= " a.done_by, ";
-$q .= " t.id ";
-$q .= " UNION ALL ";
-//show monthly tally for project across all tasks
-$q .= " SELECT  ";
-$q .= " SUM(a.hours_actual) sum_hours, ";
-$q .= " LAST_DAY(a.started) started, ";
-$q .= " SUM(a.hours_actual) done_by, ";
-$q .= " t.id, ";
-$q .= " min(t.name) name, ";
-$q .= " 1000 + min(t.task_order) ordering, ";
-$q .= " 'highlight-yellow' highlight_style ";
-$q .= $this->tables();
-$q .= $this->whereProject($id, $year, $month);
-$q .= " GROUP BY ";
-$q .= " LAST_DAY(a.started), ";
-$q .= " t.id ";
-$q .= " ORDER BY started, ordering, done_by ";
-return $q;	
-}
+	public function calendarSummaryProject($id = 0, $year = 0, $month = 0){
+		//show daily tally by task and person
+		$q = "SELECT  ";
+		$q .= " SUM(a.hours_actual) sum_hours, ";
+		$q .= " DATE(a.started) started, ";
+		$q .= " a.done_by, ";
+		$q .= " t.id, ";
+		$q .= " min(t.name) name, ";
+		$q .= " min(t.task_order) ordering, ";
+		$q .= " min(tt.highlight_style) highlight_style ";
+		$q .= $this->tables();
+		$q .= $this->whereProjectYearMonth($id, $year, $month);
+		$q .= " GROUP BY  ";
+		$q .= " DATE(a.started), ";
+		$q .= " a.done_by, ";
+		$q .= " t.id ";
+		$q .= " UNION ALL ";
+		//show monthly tally for project across all tasks
+		$q .= " SELECT  ";
+		$q .= " SUM(a.hours_actual) sum_hours, ";
+		$q .= " LAST_DAY(a.started) started, ";
+		$q .= " SUM(a.hours_actual) done_by, ";
+		$q .= " t.id, ";
+		$q .= " min(t.name) name, ";
+		$q .= " 1000 + min(t.task_order) ordering, ";
+		$q .= " 'highlight-yellow' highlight_style ";
+		$q .= $this->tables();
+		$q .= $this->whereProjectYearMonth($id, $year, $month);
+		$q .= " GROUP BY ";
+		$q .= " LAST_DAY(a.started), ";
+		$q .= " t.id ";
+		$q .= " ORDER BY started, ordering, done_by ";
+		return $q;	
+	}
 
 }
 ?>
