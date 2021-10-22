da6cffde47fee50a00c114557824cc57427250ea
Anthony Harper <gaiansentience@gmail.com>
2021-10-22 18:03:34 -0400

    input type number setting min step attributes

--- .php/framework/html/inputs/number.class.php
+++ .php/framework/html/inputs/number.class.php
@@ -2,11 +2,12 @@
 namespace html\inputs;
 
 class number extends __inputCaptioned{
-use dimensionsSize, min, max, step;
+use maxLength, min, max, step;
 	public function __construct($name, $value, $caption, $css = 'editing-input-number'){
 		parent::__construct('number',$name, $caption, $css);
 		//setDefaultDimenions
-		$this->setDimensions(12, 4);
+		$this->setMaxLength(8);
+		$this->setMax(999999);
 		$this->setValue($value);
 
 	}
@@ -17,6 +18,48 @@ class id extends number{
 
 }
 
+class percent extends __inputCaptioned{
+use min, max, step;
+	public function __construct($name, $value, $caption, $css = 'editing-input-number'){
+		parent::__construct('number',$name, $caption, $css);
+		//setDefaultDimenions
+		$this->setStep(0.01);
+		$this->setMax(1);
+		$this->setMin(0);
+		$this->setValue($value);
 
+	}
+	
+}
+
+
+class hours extends __inputCaptioned{
+use min, max, step;
+	public function __construct($name, $value, $caption, $css = 'editing-input-number'){
+		parent::__construct('number',$name, $caption, $css);
+		//setDefaultDimenions
+		$this->setStep(0.1);
+		$this->setMax(9999);
+		$this->setMin(0);
+		$this->setValue($value);
+
+	}
+	
+}
+
+
+class priority extends __inputCaptioned{
+use min, max, step;
+	public function __construct($name, $value, $caption, $css = 'editing-input-number'){
+		parent::__construct('number',$name, $caption, $css);
+		//setDefaultDimenions
+		$this->setStep(0.1);
+		$this->setMax(10);
+		$this->setMin(0);
+		$this->setValue($value);
+
+	}
+	
+}
 
 ?>
