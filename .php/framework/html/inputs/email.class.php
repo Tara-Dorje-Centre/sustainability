f104ccc491e307d7fc163e5d634701c121015726
Anthony Harper <gaiansentience@gmail.com>
2021-10-25 16:11:47 -0400

    add br and HR to inputFormSection, add br to entity edit forms

--- .php/framework/html/inputs/email.class.php
+++ .php/framework/html/inputs/email.class.php
@@ -7,7 +7,7 @@ use tooltip, disabled, dimensionsSize;
 	public function __construct($name, $value, $caption, $css = 'editing-input-email'){
 		parent::__construct('email',$name, $caption, $css);
 		//setDefaultDimenions
-		$this->setDimensions(150, 20);
+		$this->setDimensions(150, 50);
 		$this->setValue($value);
 
 	}
--- .php/framework/html/inputs/name.class.php
+++ .php/framework/html/inputs/name.class.php
@@ -6,7 +6,7 @@ class name extends text{
 	public function __construct($name, $value, $caption){
 		$css = 'editing-input-name';
 		parent::__construct($name, $value, $caption, $css);
-		$this->setDimensions(100, 20);
+		$this->setDimensions(100, 25);
 	}
 }
 
