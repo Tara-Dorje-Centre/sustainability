48d1a9cf37f4792a1ad6804cca63867653d28899
Anthony Harper <gaiansentience@gmail.com>
2021-10-26 09:58:14 -0400

    changed install file to suggest renaming .php as the working directory

--- index.php
+++ index.php
@@ -1,20 +1,17 @@
 <?php
-
-//the .php directory should be considered a library path
-//executing from here creates
-// pretty but invalid links that dont show the resource path
-// like sysLogin.php, public.php
 // 
 // .htaccess or some kind of php configuration is needed
 //  link to public.php should bring up .php/public.php
 //  without showing the user the .php/ directory
 
 include(".php/index.php");
-//   rename .php folder to the directory  to access the coe
+//   rename .php folder to the directory  to access the code
 //   for example, rename to projectManager
-//   the current code assumes all php scripts are in docroot
-//   if working code is in htdocs/dev/sustainability
-//   use [server]/dev/sustainability/.php/index.php for testing and development.
+//   then access as [server-htdocs]/projectManager/index.php
+
+//   the current code assumes all php scripts are in this directory
+//   if working code is in htdocs/dev/.php
+//   use [server-htdocs]/dev/.php/index.php for testing and development.
 //   then all links will work without any redirects
 
 ?>
